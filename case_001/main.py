5f930e798f4599ea2adb09086cc5d822d5166cbc
Chuck Conway <charles@cconway.com>
2024-07-09 06:30:39 -0700

    The first case...

---
+++ case_001/main.py
@@ -0,0 +1,7 @@
+def print_numbers(n):
+  """Prints numbers from 1 to n."""
+  for i in range(1, n):
+    print(i)
+
+# Example call
+print_numbers(5) # expecting the number printed 5 as the last number.
